bac5b13c96a753a826b18e805468e17cf15dff61
Пётр <dimok392@mail.ru>
2022-05-02 23:05:24 +0300

    example002/

---
+++ example002/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+void Print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        
+
+    }
+    Console.WriteLine();
+}
+
+int[] mass = new int[4];
+int per = -1;
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(-9, 10);
+   
+
+}
+Print(mass);
+
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = mass[i] * per;
+    
+}
+Print(mass);
